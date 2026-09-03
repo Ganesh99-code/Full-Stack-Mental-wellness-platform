5bb54abd07d2fe619c643c2af7fd5406b5634d51
Ganesh99-code <ganeshpothuraju99@gmail.com>
2026-09-04 00:27:15 +0530

    Update footer text to credit Ganesh Pothuraju

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
       <footer className="w-full text-center text-black-800 py-4 bg-green-300 font-bold fbg">
-            Made with 💙 by Shiny Toko Priya
+            Made by Ganesh Pothuraju
       </footer>
     );
   }
